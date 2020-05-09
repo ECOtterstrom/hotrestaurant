399607d4aeb6659a81904fba701afd71e7bb5604
Cole Joko <joko.cole.cj@gmail.com>
2020-05-09 10:23:26 -0600

    view.html

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let path = require('path');
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+let reservation = [];
+let waitlist = [];
+
+app.get('/', function(req, res){
+  
+})
